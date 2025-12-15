9e94d755e4ffef4098273ef63c3928c4f94c302c
J1eBuK <stefm4x@gmail.com>
2025-12-15 08:53:09 +0300

    Поправил ошибки при компиляции

--- PZ4/module.c
+++ PZ4/module.c
@@ -4,64 +4,57 @@
 #include <linux/fs.h>
 #include <linux/device.h>
 #include <linux/uaccess.h>
-#include <linux/cdev.h>
 #include <linux/ioctl.h>
 
-#define DEVICE_NAME "prikol"
-#define CLASS_NAME  "prikol_class"
-#define BUF_SIZE 20
+#define DEVICE_NAME "pz4"
+#define CLASS_NAME  "pz4_class"
+#define BUF_SIZE    20
 
 /* ioctl команды */
-#define PRIKOL_MAGIC 'p'
-#define IOCTL_CLEAR_BUFFER _IO(PRIKOL_MAGIC, 1)
-#define IOCTL_IS_EMPTY _IOR(PRIKOL_MAGIC, 2, int)
+#define PZ4_MAGIC 'p'
+#define IOCTL_CLEAR_BUFFER _IO(PZ4_MAGIC, 1)
+#define IOCTL_IS_EMPTY     _IOR(PZ4_MAGIC, 2, int)
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Max Stefanovskij");
-MODULE_DESCRIPTION("Simple driver");
+MODULE_DESCRIPTION("PZ4 character driver with buffer and ioctl");
 MODULE_VERSION("1.0");
 
-/* ===== Глобальные переменные ===== */
-
 static int major;
-static struct class *prikolClass = NULL;
-static struct device *prikolDevice = NULL;
+static struct class *pz4Class = NULL;
+static struct device *pz4Device = NULL;
 
 static char buffer[BUF_SIZE];
 static int buffer_len = 0;
 
-/* ===== Функции работы с устройством ===== */
-
-static int prikol_open(struct inode *inode, struct file *file)
+static int pz4_open(struct inode *inode, struct file *file)
 {
-    printk(KERN_INFO "prikol: device opened\n");
+    printk(KERN_INFO "pz4: device opened\n");
     return 0;
 }
 
-static int prikol_release(struct inode *inode, struct file *file)
+static int pz4_release(struct inode *inode, struct file *file)
 {
-    printk(KERN_INFO "prikol: device closed\n");
+    printk(KERN_INFO "pz4: device closed\n");
     return 0;
 }
 
-static ssize_t prikol_write(struct file *file, const char __user *user_buf,
-                            size_t len, loff_t *offset)
+static ssize_t pz4_write(struct file *file, const char __user *user_buf,
+                         size_t len, loff_t *offset)
 {
-    size_t to_copy;
-
-    to_copy = min(len, (size_t)BUF_SIZE);
+    size_t to_copy = min(len, (size_t)BUF_SIZE);
 
     if (copy_from_user(buffer, user_buf, to_copy))
         return -EFAULT;
 
     buffer_len = to_copy;
 
-    printk(KERN_INFO "prikol: wrote %d bytes\n", buffer_len);
+    printk(KERN_INFO "pz4: wrote %d bytes\n", buffer_len);
     return buffer_len;
 }
 
-static ssize_t prikol_read(struct file *file, char __user *user_buf,
-                           size_t len, loff_t *offset)
+static ssize_t pz4_read(struct file *file, char __user *user_buf,
+                        size_t len, loff_t *offset)
 {
     if (*offset >= buffer_len)
         return 0;
@@ -71,11 +64,11 @@ static ssize_t prikol_read(struct file *file, char __user *user_buf,
 
     *offset += buffer_len;
 
-    printk(KERN_INFO "prikol: read %d bytes\n", buffer_len);
+    printk(KERN_INFO "pz4: read %d bytes\n", buffer_len);
     return buffer_len;
 }
 
-static long prikol_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
+static long pz4_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
     int empty;
 
@@ -83,7 +76,7 @@ static long prikol_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
     case IOCTL_CLEAR_BUFFER:
         buffer_len = 0;
         memset(buffer, 0, BUF_SIZE);
-        printk(KERN_INFO "prikol: buffer cleared\n");
+        printk(KERN_INFO "pz4: buffer cleared\n");
         break;
 
     case IOCTL_IS_EMPTY:
@@ -99,52 +92,52 @@ static long prikol_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
     return 0;
 }
 
-/* ===== file_operations ===== */
-
 static struct file_operations fops = {
     .owner = THIS_MODULE,
-    .open = prikol_open,
-    .release = prikol_release,
-    .read = prikol_read,
-    .write = prikol_write,
-    .unlocked_ioctl = prikol_ioctl,
+    .open = pz4_open,
+    .release = pz4_release,
+    .read = pz4_read,
+    .write = pz4_write,
+    .unlocked_ioctl = pz4_ioctl,
 };
 
-static int __init prikol_init(void)
+/* ===== INIT / EXIT ===== */
+
+static int __init pz4_init(void)
 {
-    printk(KERN_INFO "prikol: initializing module\n");
+    printk(KERN_INFO "pz4: initializing module\n");
 
     major = register_chrdev(0, DEVICE_NAME, &fops);
-    if (major < 0) {
-        printk(KERN_ALERT "prikol: failed to register major\n");
+    if (major < 0)
         return major;
-    }
 
-    prikolClass = class_create(CLASS_NAME);
-    if (IS_ERR(prikolClass)) {
+    pz4Class = class_create(CLASS_NAME);
+    if (IS_ERR(pz4Class)) {
         unregister_chrdev(major, DEVICE_NAME);
-        return PTR_ERR(prikolClass);
+        return PTR_ERR(pz4Class);
     }
 
-    prikolDevice = device_create(prikolClass, NULL, MKDEV(major, 0), NULL, DEVICE_NAME);
-    if (IS_ERR(prikolDevice)) {
-        class_destroy(prikolClass);
+    pz4Device = device_create(pz4Class, NULL,
+                              MKDEV(major, 0),
+                              NULL, DEVICE_NAME);
+    if (IS_ERR(pz4Device)) {
+        class_destroy(pz4Class);
         unregister_chrdev(major, DEVICE_NAME);
-        return PTR_ERR(prikolDevice);
+        return PTR_ERR(pz4Device);
     }
 
-    printk(KERN_INFO "prikol: device created /dev/%s\n", DEVICE_NAME);
+    printk(KERN_INFO "pz4: device /dev/%s created\n", DEVICE_NAME);
     return 0;
 }
 
-static void __exit prikol_exit(void)
+static void __exit pz4_exit(void)
 {
-    device_destroy(prikolClass, MKDEV(major, 0));
-    class_destroy(prikolClass);
+    device_destroy(pz4Class, MKDEV(major, 0));
+    class_destroy(pz4Class);
     unregister_chrdev(major, DEVICE_NAME);
 
-    printk(KERN_INFO "prikol: module unloaded\n");
+    printk(KERN_INFO "pz4: module unloaded\n");
 }
 
-module_init(prikol_init);
-module_exit(prikol_exit);
+module_init(pz4_init);
+module_exit(pz4_exit);
