9bad806e024783ea74a86dd1785e39f78aeebf6c
J1eBuK <stefm4x@gmail.com>
2025-12-20 00:43:38 +0300

    Исправлен вывод проверки чист ли буфер

--- PZ4/pz4.c
+++ PZ4/pz4.c
@@ -81,7 +81,7 @@ static long pz4_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 
     case IOCTL_IS_EMPTY:
         empty = (buffer_len == 0);
-        if empty == 1
+        if (empty == 1)
               printk(KERN_INFO "pz4: buffer is empty\n");
         else
               printk(KERN_INFO "pz4: buffer is full\n");
