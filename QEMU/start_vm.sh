67b0f8e11b481b28bada55c1b9f6c70ec3fd075f
J1eBuK <stefm4x@gmail.com>
2025-12-21 23:31:56 +0300

    Изменён скрипт запуска

--- QEMU/start_vm.sh
+++ QEMU/start_vm.sh
@@ -9,12 +9,10 @@ qemu-system-x86_64 \
     -cpu max \
     -m 512 \
     -drive file=ubuntu-test.qcow2,format=qcow2,if=virtio \
-    -device isa-serial,chardev=serial0 \
-    -chardev stdio,mux=on,id=serial0,signal=off \
-    -serial chardev:serial0 \
-    -device isa-serial,chardev=serial1 \
-    -chardev socket,id=serial1,port=4445,host=0.0.0.0,server=on,wait=off \
-    -serial chardev:serial1 \
+    -device isa-serial,chardev=charserial0 \
+    -chardev stdio,mux=on,id=charserial0,signal=off \
+    -device isa-serial,chardev=charserial1 \
+    -chardev socket,id=charserial1,port=4445,host=0.0.0.0,server=on,wait=off \
     -device e1000,netdev=net0 \
     -netdev user,id=net0,hostfwd=tcp::2222-:22 \
     -display none
