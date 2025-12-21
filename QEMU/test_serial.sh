2de4b1cb1cc06503f1f50781fb2f4f1bb5865c0e
J1eBuK <stefm4x@gmail.com>
2025-12-22 02:54:57 +0300

    Изменен порядок тестирования

--- QEMU/test_serial.sh
+++ QEMU/test_serial.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 echo "=== SERIAL PORT TEST ==="
-echo "Sending packages to guest"
+echo "Sending messages..."
 
 while true; do
     echo "HOST: $(date '+%H:%M:%S')" | nc localhost 4444
     sleep 1
+    # Проверяем, работает ли соединение
+    if ! nc -z localhost 4444; then
+        echo "Connection lost, restaring..."
+        sleep 2
+    fi
 done
